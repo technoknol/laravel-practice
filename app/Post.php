033891bdbef24f9b3513511d395329339741283a
Yogesh Chaugule <shital.mahajan@wwindia.com>
2017-12-12 13:55:46 +0530

    category and post relation

--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    /**
+     * User model related to Post model
+     * @author Shyam Makwana
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function User() {
-        return $this->belongsTo(\App\User::class);
+        return $this->belongsTo(User::class);
     }
 }
