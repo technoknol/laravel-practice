393dea83fa7ea5e8c4ed2af0ada4bd5c12bcbc3c
Yogesh Chaugule <shital.mahajan@wwindia.com>
2017-12-13 12:06:46 +0530

    Add user's api_token to JavaScript's window object.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,6 +46,7 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+                            <li><a href="{{ route('register') }}"></a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
                                     {{ Auth::user()->name }} <span class="caret"></span>
@@ -75,6 +76,12 @@
     </div>
 
     <!-- Scripts -->
+    <script>
+        window.blog2 = {};
+        @auth
+            window.blog2.api_token = '{{Auth::user()->api_token}}';
+        @endauth
+    </script>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
